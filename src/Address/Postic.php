36b0d39b2075049600eea3f57079398305edd196
Tianle Xu <xtl@xtlsoft.top>
2020-01-24 17:21:33 +0800

    init: placeholder commit

---
+++ src/Address/Postic.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Lightim General Library
+ * 
+ * This repository is a part of the
+ * Lightim Project, led by xtlsoft.
+ * By xtlsoft, all rights reserved.
+ * 
+ * @author   Tianle Xu <xtl@xtlsoft.top>
+ * @license  MIT
+ * @package  lightim
+ * @category im
+ * @link     https://dev.lightim.pw
+ */
+
+namespace Lightim\Library\General\Address;
+
+class Postic implements AddressInterface
+{
+}
